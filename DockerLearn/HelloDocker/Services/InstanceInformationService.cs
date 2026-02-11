998f8879c24d6fe46f866b684fb556c79c96bfaf
IvanSSantana <ivansantospriv@gmail.com>
2026-02-10 22:53:19 -0300

    Docker Básico

---
+++ DockerLearn/HelloDocker/Services/InstanceInformationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloDocker.Services;
+
+public class InstanceInformationService
+{
+    private const string HostName = "HOSTNAME";
+    private const string DefaultEnvInstanceGuid = "LOCAL";
+
+    public string RetrieveInstanceInfo()
+    {
+        var hostName = Environment.GetEnvironmentVariable(HostName)
+            ?? DefaultEnvInstanceGuid;
+
+        return hostName.Length >= 5 ? hostName[^5..] : hostName;
+    }
+}
